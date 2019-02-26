85f4ebc78d7060abd067fca5ee897ecc6aaf15bc
renjieye07 <36244071+renjieye07@users.noreply.github.com>
2019-02-25 19:04:01 -0800

    Add files via upload
    the new schema that includes mongoose validators

--- server/models/Post.js
+++ server/models/Post.js
@@ -12,33 +12,44 @@ const postSchema = mongoose.Schema({
     },
     post_data:{
         type:Date,
-        default:Date.now
+        default:Date.now,
+        require:ture
     },
     post_joined_user:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
     }],//list of use id 
-    post_infor:String,
+    post_infor:{
+        type:String,
+        minlength:8,
+        maxlength:10000,
+    },
     photo_id:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Photo'
     }],//list of photo id
     post_title:{
         type:String,
-        require:ture
+        require:ture,
+        minlength:8,
+        maxlength:200,
     },
     post_like:Number,
     post_dislike:Number,
-    post_active:Boolean,
+    post_active:{
+        type:Boolean,
+        default:ture
+    },
     post_host:{//the owner of the post
         type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
+        ref:'User',
+        require:ture
     },
 
-    user_id:[{
+    comment_user_id:[{
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
     }]//list of user id who comment on the post??? 
 });
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model('Review', reviewSchema);
