0dc67dd221865ce3cf711631d6c218ecc3811fd9
romanDQ <32752839+romanDQ@users.noreply.github.com>
2019-03-13 21:07:16 -0700

    Add files via upload

---
+++ frontend/src/features/home/HomePage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const HomePage = ({history}) => {
+  return (
+    <div>
+      <div className="ui inverted vertical masthead center aligned segment">
+        <div className="ui text container">
+          <h1 className="ui inverted stackable header">
+            <img
+              className="ui image massive"
+              src="/assets/logo.png"
+              alt="logo"
+            />
+            <div className="content">NeighborGood</div>
+          </h1>
+          <h2>Enjoy the smart and convenient Community</h2>
+          <div onClick={() => history.push('/Signup')} className="ui huge white inverted button">
+            Get Started
+            <i className="right arrow icon" />
+          </div>
+        </div>
+      </div>
+      <div style={{ textAlign: 'center' }}>
+      
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
